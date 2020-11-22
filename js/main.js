f3656ac2b290aacef20b955c75f4353b3f3f3716
AlanSmith198 <lazarmakulovic.98@gmail.com>
2020-11-22 23:53:25 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.hamburger-menu').click(function(event){
+    event.stopPropagation();
+    if ($('main').hasClass('active')){
+        $('main').removeClass('active');
+    } else {
+        $('main').addClass('active');
+    }
+});
+$('#blog').waypoint(function(direction){
+    if(direction == 'down') {
+        $('header').addClass('sticky');
+    } else {
+        $('header').removeClass('sticky');
+    }
+});
